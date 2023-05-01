41709a88187d69a3bb9256faa02b7585090b2b43
jolikecat <jkawakubo@contenna.jp>
2023-05-01 19:11:40 +0900

    :adhesive_bandage:element が null である可能性を考慮する

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,15 +2,18 @@
  * 画面幅 < minWidth の時にviewportを固定
  */
 const minWidth = 375;
-const el = document.querySelector('meta[name="viewport"]');
-const updateContent = () => {
-	if (window.screen.width < minWidth) {
-		el.setAttribute('content', `width=${minWidth}`);
-	} else {
-		el.setAttribute('content', 'width=device-width');
-	}
-};
+const element = document.querySelector('meta[name="viewport"]');
 
-updateContent();
+if (element) {
+	const updateContent = () => {
+		if (window.screen.width < minWidth) {
+			element.setAttribute('content', `width=${minWidth}`);
+		} else {
+			element.setAttribute('content', 'width=device-width');
+		}
+	};
 
-window.addEventListener('orientationchange', updateContent);
+	updateContent();
+
+	window.addEventListener('orientationchange', updateContent);
+}
