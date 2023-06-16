7ca729864ad8fdcab37e7100b077336c17e1217d
jolikecat <jkawakubo@contenna.jp>
2023-06-16 16:18:08 +0900

    :feat:prettierを追加、stylelintのルールを更新

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @type { import('prettier').Config }
+ */
+module.exports = {
+	printWidth: Number.POSITIVE_INFINITY,
+	trailingComma: 'all',
+	singleQuote: true,
+	useTabs: true,
+};
